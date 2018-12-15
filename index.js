09f8d6b7b3a8331355222a9ec513ed75aa778e7f
George Benjamin <msdcconnect@gmail.com>
2018-12-16 00:59:48 +0100

    Handled exceptions

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 module.exports.pick = (arr, n) => {
-    let arrc = [];
-    let i = arr.length;
-    while(i--) arrc[i] = arr[i];
-    console.log(arrc)
-    let selected = [];
-    for(let i = 0; i < n; i++){
-        var r = Math.floor(Math.random() * arrc.length)
-        selected.push(arrc[r]);
-        arrc.splice(r, 1);
+    if(typeof(arr) === 'undefined'){
+        throw new Error('Illegal argument exception - array is not defined.');
     }
-    return selected;
-}
 
-/*
-function pick(arr, n){
+    if(typeof(n) === 'undefined'){
+        throw new Error('Illegal argument exception - n is not defined.')
+    }
+
+    if(arr === null){
+        throw new Error('Illegal argument exception  - array cannot be null.');
+    }
+
+    if(arr.length < 1){
+        throw new Error('Illegal argument exception - array must at least a length of one.')
+    }
+
+    if(n < 1){
+        throw new Error('Illegal argument exception - n canot be less than 1.');
+    }
+
+    if(typeof(n) !== 'number'){
+        throw new Error('Type Error - n must be a number.');
+    }
+
+    if(n > arr.length){
+        throw new Error('Illegal argument exception - n cannot be greater than the length of the array.');
+    }
+
     let arrc = [];
     let i = arr.length;
     while(i--) arrc[i] = arr[i];
-    console.log(arrc)
     let selected = [];
     for(let i = 0; i < n; i++){
         var r = Math.floor(Math.random() * arrc.length)
@@ -26,4 +38,3 @@ function pick(arr, n){
     }
     return selected;
 }
-*/
